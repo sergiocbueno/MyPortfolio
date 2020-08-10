904beac9da6b1b520056e5c1e9b27e3ab67f3b88
Sergio Bueno <sergiocbueno@gmail.com>
2020-08-10 22:54:45 +0200

    Fix active menu option bug

--- MyPortfolio/wwwroot/js/resume.js
+++ MyPortfolio/wwwroot/js/resume.js
@@ -1,28 +1,54 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
+  $(document).ready(function() {
+		var page = window.location.href.substring(window.location.href.lastIndexOf('/') + 1).toLowerCase();
+		switch(page){
+			case '':
+			case 'home':
+      case 'about':
+				$('#experience_page').removeClass('active');
+				$('#education_page').removeClass('active');
+				$('#skills_page').removeClass('active');
+				$('#mapping_page').removeClass('active');
+				$('#about_page').addClass('active');
+				break;
+			case 'experience':
+				$('#about_page').removeClass('active');
+				$('#education_page').removeClass('active');
+				$('#skills_page').removeClass('active');
+				$('#mapping_page').removeClass('active');
+				$('#experience_page').addClass('active');
+				break;
+			case 'education':
+				$('#about_page').removeClass('active');
+				$('#experience_page').removeClass('active');
+				$('#skills_page').removeClass('active');
+				$('#mapping_page').removeClass('active');
+				$('#education_page').addClass('active');
+				break;
+			case 'skills':
+				$('#about_page').removeClass('active');
+				$('#experience_page').removeClass('active');
+				$('#education_page').removeClass('active');
+				$('#mapping_page').removeClass('active');
+				$('#skills_page').addClass('active');
+				break;
+			case 'mappingaccess':
+				$('#about_page').removeClass('active');
+				$('#experience_page').removeClass('active');
+				$('#education_page').removeClass('active');
+				$('#skills_page').removeClass('active');
+				$('#mapping_page').addClass('active');
+				break;
+			default:
+				$('#about_page').removeClass('active');
+				$('#experience_page').removeClass('active');
+				$('#education_page').removeClass('active');
+				$('#skills_page').removeClass('active');
+				$('#mapping_page').removeClass('active');
+				break;
+		}
   });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
-  });
-
+  
 })(jQuery); // End of use strict
